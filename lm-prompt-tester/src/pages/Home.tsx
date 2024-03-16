87b9f02ab15b2790d574872b8f16219af17799cb
gangjoohyeong <bles@kakao.com>
2024-03-16 13:05:34 +0900

    react: 소개 페이지 구현

--- lm-prompt-tester/src/pages/Home.tsx
+++ lm-prompt-tester/src/pages/Home.tsx
@@ -1,5 +1,26 @@
+import styles from "./Home.module.css";
+
 const Home = (): JSX.Element => {
-  return <div>Home</div>;
+  return (
+    <div className={styles.introContainer}>
+      <h1 className={styles.title}>Welcome to LM Prompt Tester</h1>
+      <p className={styles.description}>
+        "LM Prompt Tester" is a web platform designed for testing various
+        language models such as GPT, PALM 2, Gemma, and more. Users can input
+        prompts and observe the generated outputs of these models for analysis
+        and experimentation. It allows users to quickly assess how different
+        language models respond to different prompts, facilitating better
+        understanding and adjustment of the models.
+        <br />
+        <br />
+        Additionally, LM Prompt Tester enables performance comparisons between
+        different platforms and language models, providing valuable insights for
+        researchers, developers, and NLP engineers. Explore LM Prompt Tester now
+        to experiment with your projects and delve into the capabilities of
+        language models!
+      </p>
+    </div>
+  );
 };
 
 export default Home;
