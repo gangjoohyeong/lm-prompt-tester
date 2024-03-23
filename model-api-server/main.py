8b95472f0993e44ca9a6ff47795433ffdb6ffad1
gangjoohyeong <bles@kakao.com>
2024-03-23 09:08:47 +0900

    fastapi: CORS 개방

--- model-api-server/main.py
+++ model-api-server/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 from api import api_key, openai, anthropic
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def hello():
